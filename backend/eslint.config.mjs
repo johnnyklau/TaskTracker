f0ea09c5805a1bb7f79aee758fbd9fe119a85b6b
Johnny Lau <83278561+FeeshSushi@users.noreply.github.com>
2026-09-16 15:45:12 -0700

    Linter adjustment

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,11 +1,15 @@
-import { defineConfig } from 'eslint/config';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default defineConfig(
+export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
     ignores: ['dist', 'node_modules'],
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
   }
 );
